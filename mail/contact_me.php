a860819d80016a713ad98fcee97e9fd35b7a0f73
Roger Gonzalez <rogergonzalez21@gmail.com>
2019-07-13 19:23:28 -0400

    Fixed typo

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -12,7 +12,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
 $to = "derys@deryssevilla.design"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
-$subject = "Nuevo contacto de derissevilla.design  $name";
+$subject = "Nuevo contacto de deryssevilla.design  $name";
 $body = "Has recibido un nuevo mensaje en el formulario de contacto.\n\n"."Aquí están los detalles:\n\nNombre: $name\n\nEmail: $email\n\nTeléfono: $phone\n\nMensaje:\n$message";
 $header = "From: noreply@deryssevilla.design\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
